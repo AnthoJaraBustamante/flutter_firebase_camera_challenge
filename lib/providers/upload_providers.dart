080c33a6c066b762d28048da4639666b3a8767ea
AnthoJaraBustamante <omarbush81@gmail.com>
2024-03-06 16:22:24 -0500

    Actualizar proveedor de cargas a un StreamProvider

--- lib/providers/upload_providers.dart
+++ lib/providers/upload_providers.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../services/firebase_service.dart';
 
-final uploadsProvider = FutureProvider<List<DocumentSnapshot>>((ref) async {
-  final docs = await getUploads();
-  return docs;
+final uploadsProvider = StreamProvider<List<DocumentSnapshot>>((ref) {
+  return getUploads();
 });
